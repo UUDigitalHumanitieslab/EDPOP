fe1be641d9afe5b9fe62a3d94d774ffe02a4dfdf
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-03-27 12:26:11 +0200

    Make filtering by "own" collections optional (#31, #41)
    /vre/api/collections/       --> all collections
    /vre/api/collections/mine/  --> collections managed by my groups

--- vre/api.py
+++ vre/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
 
 from .serializers import *
 from .models import Collection, Record
@@ -7,9 +8,17 @@ from .models import Collection, Record
 class CollectionViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CollectionSerializer
 
+    @list_route()
+    def mine(self, request):
+        # get_queryset takes care of the reduced queryset
+        return self.list(request)
+
     def get_queryset(self):
-        groups = self.request.user.researchgroups.all()
-        return Collection.objects.filter(managing_group__in=groups)
+        queryset = Collection.objects
+        if self.action == 'mine':
+            groups = self.request.user.researchgroups.all()
+            return queryset.filter(managing_group__in=groups)
+        return queryset.all()
 
 
 class RecordViewSet(viewsets.ReadOnlyModelViewSet):
