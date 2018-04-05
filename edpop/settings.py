154756d6cc5afa2b29ae23f95a01e6c0a257983b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-04-05 11:48:53 +0200

    Require authentication on the API (#31, #41)

--- edpop/settings.py
+++ edpop/settings.py
@@ -113,6 +113,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
