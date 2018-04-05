d0fb77ed9a461751263bc7c95abfbb9c9831bd59
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-04-05 17:03:11 +0200

    Add serialization for the ResearchGroup and Annotation models (#28, #41)

--- vre/serializers.py
+++ vre/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
-from .models import Collection, Record
+from .models import ResearchGroup, Collection, Record, Annotation
+
+
+class ResearchGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResearchGroup
+        fields = ('id', 'name', 'project')
 
 
 class CollectionSerializer(serializers.ModelSerializer):
+    managing_group = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Collection
-        fields = ('id', 'description')
+        fields = ('id', 'description', 'managing_group')
 
 
 class RecordSerializer(serializers.ModelSerializer):
@@ -15,3 +23,12 @@ class RecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Record
         fields = ('id', 'uri', 'collection', 'content')
+
+
+class AnnotationSerializer(serializers.ModelSerializer):
+    record = serializers.PrimaryKeyRelatedField(read_only=True)
+    managing_group = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Annotation
+        fields = ('id', 'record', 'managing_group', 'content')
