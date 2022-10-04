3c47dd0f02eb839b438a62c75e90f94ee8f4f6eb
Julian Gonggrijp <j.gonggrijp@uu.nl>
2022-10-04 19:39:38 +0200

    Add a preliminary backend sanity check (#116 #111)

---
+++ vre/api_test.py
@@ -0,0 +1,68 @@
+import pytest
+
+import json
+
+from .models import Record, Collection
+
+
+# TODO: move the constants and fixtures below to a conftest.py
+
+RECORDS = [
+    {'uri': 'http://testing.test/1', 'content': {}},
+    {'uri': 'http://testing.test/2', 'content': {}},
+]
+
+COLLECTIONS = [
+    {'description': 'fun collection'},
+    {'description': 'serious collection'},
+]
+
+
+@pytest.fixture
+def records(db):
+    for data in RECORDS:
+        Record(**data).save()
+    yield Record.objects
+    Record.objects.all().delete()
+
+
+@pytest.fixture
+def collections(db):
+    for data in COLLECTIONS:
+        Collection(**data).save()
+    yield Collection.objects
+    Collection.objects.all().delete()
+
+
+@pytest.fixture
+def auth_user(django_user_model, db):
+    name = 'tester'
+    pw = 'testing 123'
+    user = django_user_model.objects.create_user(username=name, password=pw)
+    yield user, name, pw
+    user.delete()
+
+
+@pytest.fixture
+def auth_client(auth_user, client):
+    user, name, pw = auth_user
+    success = client.login(username=name, password=pw)
+    assert success
+    yield client
+    client.logout()
+
+
+def test_add_single_record_preexisting(auth_client, records, collections):
+    record = records.first()
+    collection = collections.first()
+    payload = {
+        'records': [{'uri': record.uri, 'content': record.content}],
+        'collections': [collection.pk]
+    }
+    response = auth_client.post('/vre/api/add-selection/',
+        data=json.dumps(payload),
+        content_type='application/json',
+    )
+    assert response.status_code is 200
+    assert response.json() == {collection.description: 1}
+    assert record.collection.get(pk=collection.pk)
