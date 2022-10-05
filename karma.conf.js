23fc0975fdda6d3e5fc127a2713678b52a13ad86
Julian Gonggrijp <j.gonggrijp@uu.nl>
2022-10-05 17:20:52 +0200

    Enable frontend testing with karma, mocha and power-assert (#117)

---
+++ karma.conf.js
@@ -0,0 +1,73 @@
+// Karma configuration
+// Generated on Wed Oct 05 2022 17:04:26 GMT+0200 (Central European Summer Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    frameworks: ['browserify', 'mocha', 'power-assert'],
+
+    // list of files / patterns to load in the browser
+    files: [
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/lodash/lodash.js',
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/select2/dist/js/select2.js',
+        'vre/static/vre/**/*.test.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    preprocessors: {
+        'vre/static/vre/**/*.test.js': ['browserify']
+    },
+
+    browserify: {
+        transform: [['babelify', {
+            presets: 'power-assert'
+        }], ['browserify-shim', {
+            jquery: 'global:$',
+            lodash: 'global:_'
+        }]]
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: [],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser instances should be started simultaneously
+    concurrency: Infinity
+  })
+}
