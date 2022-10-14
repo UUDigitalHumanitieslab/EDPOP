f23761e71ee03fc5d5bccbb71272dfa11c8512ff
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-10-14 18:00:25 +0200

    Upgrade to Django 3.0; djangorestframework 3.14.0 (latest version)

--- vre/views.py
+++ vre/views.py
@@ -1,14 +1,8 @@
-import json
-
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, render_to_response
-from django.core import serializers
+from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer
 
-from .sru_query import sru_query, translate_sru_response_to_dict
-from .models import Record, ResearchGroup, Collection
+from .models import ResearchGroup, Collection
 from .serializers import CollectionSerializer, ResearchGroupSerializer
 
 CERL_SRU_URL = "http://sru.cerl.org/thesaurus"
@@ -25,8 +19,11 @@ def index(request, database_id=None):
     prefetched_collections = CollectionSerializer(user_collections, many=True)
     prefetched_groups = ResearchGroupSerializer(all_groups, many=True)
     response_dict = {
-        'prefetched_collections': JSONRenderer().render(prefetched_collections.data),
-        'prefetched_groups': JSONRenderer().render(prefetched_groups.data)}
+        'prefetched_collections':
+            JSONRenderer().render(prefetched_collections.data),
+        'prefetched_groups':
+            JSONRenderer().render(prefetched_groups.data)
+    }
     if database_id:
         response_dict['id'] = database_id
     return render(
