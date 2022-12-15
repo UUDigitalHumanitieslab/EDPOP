d9a2c74f8f223424a62513737d88edaa607fda46
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-12-15 12:06:24 +0100

    Cap response in log message to 500 chars

--- vre/sru_query.py
+++ vre/sru_query.py
@@ -50,7 +50,8 @@ def sru_query(url_string, query_string, startRecord=1, sru_version='1.1'):
 
 def translate_sru_response_to_dict(response_content):
     logger.info('Translating SRU response to dict')
-    logger.info('SRU response:\n{}'.format(str(response_content)))
+    logger.info('SRU response type: ' + str(type(response_content)))
+    logger.info('SRU response:\n{}'.format(response_content[:500]))
     translationDictionary = load_translation_dictionary()
     soup = BeautifulSoup(response_content, 'lxml')
     diagnostic = soup.find('diag:message')
