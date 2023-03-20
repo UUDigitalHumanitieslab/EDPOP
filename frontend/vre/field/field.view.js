54061bdae3df893f95a21d272dc76b0ab430e6d6
Julian Gonggrijp <j.gonggrijp@uu.nl>
2023-03-20 19:49:04 +0100

    Give frontend FieldView more breathing room

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -7,16 +7,20 @@ import fieldTemplate from './field.view.mustache';
 export var FieldView = View.extend({
     tagName: 'tr',
     template: fieldTemplate,
+
     events: {
         'click': 'edit',
     },
+
     initialize: function(options) {
         this.render().listenTo(this.model, 'change:value', this.render);
     },
+
     render: function() {
         this.$el.html(this.template(this.model.attributes));
         return this;
     },
+
     edit: function(event) {
         this.trigger('edit', this.model);
     },
