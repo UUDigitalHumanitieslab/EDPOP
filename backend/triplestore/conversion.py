be32d475a03ac55ad5c9d47df08b8beb6a761a86
Luka van der Plas <lukavdplas@gmail.com>
2023-09-27 17:42:05 +0200

    add groups

--- backend/triplestore/conversion.py
+++ backend/triplestore/conversion.py
@@ -1,7 +1,15 @@
 from rdflib import Graph, RDF, BNode, Literal
 
-from vre.models import Collection
+from vre.models import ResearchGroup, Collection
 from .constants import EDPOPCOL, AS
+
+def add_project_to_graph(research_group: ResearchGroup, g: Graph):
+    subject = BNode()
+
+    g.add((subject, RDF.type, EDPOPCOL.Project))
+
+    name = Literal(research_group.project)
+    g.add((subject, AS.name, name))
 
 def add_collection_to_graph(collection: Collection, g: Graph):
     subject = BNode()
@@ -12,5 +20,7 @@ def add_collection_to_graph(collection: Collection, g: Graph):
         description = Literal(collection.description)
         g.add((subject, AS.summary, description))
 
-    return g
+    for group in collection.managing_group.all():
+        pass
 
+    return g
--- backend/triplestore/conversion_test.py
+++ backend/triplestore/conversion_test.py
@@ -1,25 +1,52 @@
 from typing import Tuple
 from rdflib import RDF, Literal, Graph, URIRef
+import pytest
 
-from vre.models import Collection
+from vre.models import ResearchGroup, Collection
 from .constants import EDPOPCOL, AS
-from .conversion import add_collection_to_graph
+from .conversion import add_project_to_graph, add_collection_to_graph
 
 def triple_exists(graph: Graph, triple: Tuple[URIRef]):
     return any(graph.triples(triple))
 
-def test_add_collection_to_graph(empty_graph):
+def find_triple(graph: Graph, triple: Tuple[URIRef]):
+    return next(graph.triples(triple))
+
+def find_subject(graph: Graph, predicate: URIRef, object: URIRef):
+    return find_triple(graph, (None, predicate, object))
+
+@pytest.fixture()
+def fake_group(db):
+    group = ResearchGroup.objects.create(
+        name='test researchers',
+        project='test project'
+    )
+    return group
+
+def test_add_project_to_graph(fake_group, empty_graph):
+    g = empty_graph
+    add_project_to_graph(fake_group, g)
+
+    assert triple_exists(g, (None, RDF.type, EDPOPCOL.Project))
+
+@pytest.fixture()
+def fake_collection(db, fake_group):
+    collection = Collection.objects.create(
+        description='a collection for testing',
+    )
+    collection.managing_group.add(fake_group)
+    collection.save()
+    return collection
+
+def test_add_collection_to_graph(fake_group, fake_collection, empty_graph):
     g = empty_graph
 
-    collection = Collection(
-        description = 'a collection for testing'
-    )
-
-    add_collection_to_graph(collection, g)
+    add_project_to_graph(fake_group, g)
+    add_collection_to_graph(fake_collection, g)
     
     assert triple_exists(g, (None, RDF.type, EDPOPCOL.Collection))
 
-    collection, _, _ = next(g.triples((None, RDF.type, EDPOPCOL.Collection)))
+    collection, _, _ = find_subject(g, RDF.type, EDPOPCOL.Collection)
 
     summary_triple = (collection, AS.summary, Literal('a collection for testing'))
     assert triple_exists(g, summary_triple)
