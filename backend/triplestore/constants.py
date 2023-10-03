02d7efd3acb642dd00e16c3fec616c2f0480b8c3
Luka van der Plas <lukavdplas@gmail.com>
2023-10-03 15:31:45 +0200

    add record ontology namespace

--- backend/triplestore/constants.py
+++ backend/triplestore/constants.py
@@ -2,10 +2,13 @@ from rdflib.namespace import Namespace
 
 
 EDPOPCOL = Namespace('https://dhstatic.hum.uu.nl/edpop-collection/latest#')
-"""EDPOP Collection Ontology"""
+'''EDPOP Collection Ontology'''
+
+EDPOPREC = Namespace('https://dhstatic.hum.uu.nl/edpop-records/latest/')
+'''EDPOP Record Ontology'''
 
 AS = Namespace('https://www.w3.org/ns/activitystreams#')
-"""Activity Streams vocabluary"""
+'''Activity Streams vocabluary'''
 
 OA = Namespace('http://www.w3.org/ns/oa#')
-"""Web Annotation vocabulary"""
+'''Web Annotation vocabulary'''
