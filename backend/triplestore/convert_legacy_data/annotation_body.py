7070f09857a82b476c7909b5cc5c61684ac1415d
Luka van der Plas <lukavdplas@gmail.com>
2023-10-24 10:59:07 +0200

    start removal annotation

--- backend/triplestore/convert_legacy_data/annotation_body.py
+++ backend/triplestore/convert_legacy_data/annotation_body.py
@@ -1,5 +1,5 @@
 from rdflib import URIRef, Graph, BNode, RDF
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Set
 
 from vre.models import Annotation
 from triplestore.constants import OA, EDPOPCOL
@@ -9,6 +9,7 @@ from .import_legacy_records import field_to_graph
 def annotation_body_to_graph(annotation: Annotation, annotation_uri: URIRef, property_uris: ObjectURIs) -> Graph:
     body, g = _blank_body(annotation_uri)
     g += _additions_to_graph(annotation.content, body, property_uris)
+    g += _removals_to_graph(annotation.content, annotation.record.content, body, property_uris)
     return g
 
 def _blank_body(annotation_uri) -> Tuple[URIRef, Graph]:
@@ -23,13 +24,31 @@ def _blank_body(annotation_uri) -> Tuple[URIRef, Graph]:
 def _additions_to_graph(annotation_content: Dict[str, str], body: URIRef, property_uris: ObjectURIs) -> Graph:
     g = Graph()
     for (key, value) in annotation_content.items():
-        predicate, predicate_graph = _annotation_item_to_graph(key, value, property_uris)
+        field, field_graph = field_to_graph(value)
+        predicate, predicate_graph = _annotation_item_to_graph(key, field, property_uris)
+        g += field_graph
         g += predicate_graph
         g.add((body, EDPOPCOL.suggestsAddition, predicate))
 
     return g
 
-def _annotation_item_to_graph(key: str, value: str, property_uris: ObjectURIs) -> Tuple[URIRef, Graph]:
+def _removals_to_graph(annotation_content: Dict[str, str], record_content: Dict[str, str], body: URIRef, property_uris: ObjectURIs) -> Graph:
+    g = Graph()
+
+    for label in _replaced_labels(annotation_content, record_content):
+        field = BNode() # TODO get current field
+        predicate, predicate_graph = _annotation_item_to_graph(label, field, property_uris)
+        g += predicate_graph
+        g.add((body, EDPOPCOL.suggestsRemoval, predicate))
+
+    return g
+
+def _replaced_labels(annotation_content: Dict[str, str], record_content: Dict[str, str]) -> Set[str]:
+    annotation_labels = annotation_content.keys()
+    record_labels = record_content.keys()
+    return set(annotation_labels).intersection(record_labels)
+
+def _annotation_item_to_graph(key: str, field: URIRef, property_uris: ObjectURIs) -> Tuple[URIRef, Graph]:
     g = Graph()
 
     subject = BNode()
@@ -37,9 +56,6 @@ def _annotation_item_to_graph(key: str, value: str, property_uris: ObjectURIs) -
 
     property = property_uris[key]
     g.add((subject, EDPOPCOL.property, property))
-
-    field, field_graph = field_to_graph(value)
-    g += field_graph
     g.add((subject, EDPOPCOL.object, field))
 
     return subject, g
--- backend/triplestore/convert_legacy_data/annotation_body_test.py
+++ backend/triplestore/convert_legacy_data/annotation_body_test.py
@@ -1,8 +1,8 @@
 from rdflib import BNode
 import pytest
 
-from .annotation_body import annotation_body_to_graph
-from triplestore.utils import find_subject_by_class, triple_exists
+from .annotation_body import annotation_body_to_graph, _replaced_labels
+from triplestore.utils import find_subject_by_class
 from triplestore.constants import EDPOPCOL
 
 
@@ -14,4 +14,16 @@ def test_annotation_body_to_graph(fake_annotation, property_uris):
     assert g
 
     body = find_subject_by_class(g, EDPOPCOL.AnnotationBody)
-    assert triple_exists(g, (body, EDPOPCOL.suggestsAddition, None))
+    additions = list(g.objects(body, EDPOPCOL.suggestsAddition))
+    removals = list(g.objects(body, EDPOPCOL.suggestsRemoval))
+
+    assert len(additions) == 2
+    assert len(removals) == 1
+
+def test_replaced_labels():
+    replaced = _replaced_labels(
+        {'a': '1', 'b': '2'},
+        {'b': '3', 'c': '4'}
+    )
+
+    assert replaced == {'b'}
