5c20dffb0c0144cf3f01b6ad01571e83d17bc663
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-17 12:08:08 +0200

    use horizontal widget in admin form

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -3,6 +3,8 @@ from projects import models
 
 @admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ['users', 'groups']
+
     def get_readonly_fields(self, request, obj):
         # make name readonly after creation to prevent breaking IRIs
         return ['name'] if obj else []
