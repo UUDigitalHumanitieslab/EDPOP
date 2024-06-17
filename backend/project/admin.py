239e0222f17dc600ff3a0360e4075781db7a75c2
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-17 12:03:16 +0200

    make project name readonly in admin

--- backend/project/admin.py
+++ backend/project/admin.py
@@ -3,4 +3,6 @@ from project import models
 
 @admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    def get_readonly_fields(self, request, obj):
+        # make name readonly after creation to prevent breaking IRIs
+        return ['name'] if obj else []
