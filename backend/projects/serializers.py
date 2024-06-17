cc754624bf3b202d14e639d486a1eae02eb556ca
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-17 12:36:52 +0200

    more thorough project view tests

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -5,3 +5,4 @@ from projects.models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = ['name', 'display_name', 'summary']
--- backend/projects/views_test.py
+++ backend/projects/views_test.py
@@ -1,5 +1,41 @@
 from django.test import Client
+from django.contrib.auth.models import User
+import pytest
 
-def test_project_list_view(db, client: Client):
+from projects.models import Project
+
+@pytest.fixture()
+def user(db):
+    return User.objects.create(
+        username='tester',
+        password='secret'
+    )
+
+@pytest.fixture()
+def public_project(db):
+    return Project.objects.create(
+        name='public',
+        display_name='Public',
+        public=True,
+    )
+
+@pytest.fixture()
+def private_project(db, user):
+    project = Project.objects.create(
+        name='private',
+        display_name='Private',
+    )
+    project.users.add(user)
+    project.save()
+    return project
+
+def test_project_list_view_anonymous(db, public_project, private_project, client: Client):
     response = client.get('/api/projects/')
     assert response.status_code == 200
+    assert len(response.data) == 1
+
+def test_project_list_view_authenticated(db, public_project, private_project, user, client: Client):
+    client.force_login(user)
+    response = client.get('/api/projects/')
+    assert response.status_code == 200
+    assert len(response.data) == 2
