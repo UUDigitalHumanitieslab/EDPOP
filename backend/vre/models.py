5f9dda6f756f8305c043af433656d75a6dfe4e64
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-18 15:32:42 +0200

    remove researchgroup model

--- backend/vre/models.py
+++ backend/vre/models.py
@@ -3,30 +3,12 @@ from django.contrib.auth.models import User
 
 from projects.models import Project
 
-class ResearchGroup(models.Model):
-    """ Research group with a given name and project;
-    Users can be part of multiple research groups,
-    research groups have multiple users.
-    """
-    name = models.CharField(max_length=200)
-    project = models.CharField(max_length=200)
-    members = models.ManyToManyField(User, related_name='researchgroups')
-
-    class Meta:
-        permissions = (
-            ("add_user_to_research_group", "Can add user to a research group"),
-        )
-
-    def __str__(self):
-        return self.name
-
 
 class Collection(models.Model):
     """ a collection of records imported from an external resource,
     which can be annotated and extended in the Virtual Research Environment.
     """
     description = models.CharField(max_length=200)
-    managing_group = models.ManyToManyField(ResearchGroup)
     context = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     class Meta:
@@ -58,9 +40,8 @@ class Annotation(models.Model):
     An annotation is also linked to exactly one research group,
     but multiple groups can add annotations."""
     record = models.ForeignKey(Record, on_delete=models.CASCADE)
-    managing_group = models.ForeignKey(ResearchGroup, on_delete=models.CASCADE)
     context = models.ForeignKey(Project, on_delete=models.CASCADE)
     content = models.JSONField(dict)
 
     def __str__(self):
-        return '{} ({})'.format(self.record.uri, self.managing_group.name)
+        return '{} ({})'.format(self.record.uri, self.context.name)
