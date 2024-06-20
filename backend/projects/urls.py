3aef6b986359af8f7df50132c494a8d28ddc3de5
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-20 15:57:09 +0200

    rename views.py -> api.py

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from projects import views
+from backend.projects import api
 
 urlpatterns = [
-    path('api/projects/', views.ProjectView.as_view({'get': 'list'})),
+    path('api/projects/', api.ProjectView.as_view({'get': 'list'})),
 ]
