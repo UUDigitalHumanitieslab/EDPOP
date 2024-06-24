6f359f520e0fe203ace8fac3799da6d145cfabce
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-24 21:38:37 +0200

    update imports

--- backend/projects/rdf_models.py
+++ backend/projects/rdf_models.py
@@ -1,33 +1,8 @@
-from rdflib import Literal
-
 from triplestore.constants import AS, EDPOPCOL
-from triplestore.rdf_model import (
-    RDFModel, RDFClassField, RDFUniquePropertyField
-)
-
-class ASNameField(RDFUniquePropertyField):
-    def __init__(self):
-        super().__init__(AS.name)()
-
-    def value_to_node(self, value: str) -> Literal:
-        return Literal(value)
-
-    def node_to_value(self, node: Literal):
-        return node.value
-
-
-class ASSummaryField(RDFUniquePropertyField):
-    def __init__(self, ):
-        super().__init__(AS.summary)()
-
-    def value_to_node(self, value: str) -> Literal:
-        return Literal(value)
-
-    def node_to_value(self, node: Literal):
-        return node.value
-
+from triplestore.rdf_model import RDFModel
+from triplestore.rdf_field import RDFUniquePropertyField, RDFClassField
 
 class RDFProject(RDFModel):
     project_class = RDFClassField(EDPOPCOL.Project)
-    name = ASNameField()
-    summary = ASSummaryField()
+    name = RDFUniquePropertyField(AS.name)
+    summary = RDFUniquePropertyField(AS.summary)
