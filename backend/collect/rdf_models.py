85389d88d889f5c12a86dbb625e2a55e4e2dfe73
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-25 16:21:41 +0200

    add projects and records fields

--- backend/collect/rdf_models.py
+++ backend/collect/rdf_models.py
@@ -1,48 +1,50 @@
-from rdflib.term import Identifier, IdentifiedNode
-from rdflib import Graph, RDFS
+from rdflib import Graph, RDFS, IdentifiedNode
+from typing import Iterable
 
 from triplestore.utils import Triples
-from triplestore.constants import EDPOPCOL, AS, EDPOPREC
+from triplestore.constants import EDPOPCOL, AS
 from triplestore.rdf_model import RDFModel
-from triplestore.rdf_field import RDFField, RDFUniquePropertyField, RDFPropertyField
+from triplestore.rdf_field import RDFField, RDFUniquePropertyField
 from projects.rdf_models import RDFProject
 
-# class ProjectContextField(RDFPropertyField):
-#     def __init__(self):
-#         super().init(AS.context)
-    
-#     def node_to_value(self, node: Identifier):
-#         if isinstance(node, IdentifiedNode):
-#             return RDFProject(None, node)
+class ProjectContextField(RDFField):    
+    def get(self, graph: Graph, instance: RDFModel):
+        return [
+            RDFProject(o)
+            for (s, p, o) in self._stored_triples(graph, instance)
+        ]
 
-#     def value_to_node(self, value: RDFProject) -> Identifier:
-#         return value.uri
+    def _stored_triples(self, g: Graph, instance: RDFModel) -> Triples:
+        return g.triples((instance.uri, AS.context, None))
+
+    def _triples_to_store(self, g: Graph, instance: RDFModel, value: Iterable[RDFProject]) -> Triples:
+        return [
+            (instance.uri, AS.context, project.uri)
+            for project in value
+        ]
 
 
-# class CollectionMembers(RDFField):
-#     def get(self, g: Graph, instance):
-#         for triple in g.triples((None, RDFS.member, instance.uri)):
-#             print(triple)
+class CollectionMembersField(RDFField):
+    def get(self, graph: Graph, instance: RDFModel):
+        return [
+            RDFProject(s)
+            for (s, p, o) in self._stored_triples(graph, instance)
+        ]
 
-#         return None
+    def _stored_triples(self, g: Graph, instance: RDFModel) -> Triples:
+        return g.triples((None, RDFS.member, instance.uri))
 
-#         # query = f'''
-#         # SELECT ?record
-#         # WHERE {{
-#         #     ?record     a edpoprec:Record ;
-#         #                 rdfs:member {instance.uri} .
-#         # }}
-#         # '''
-#         # namespaces = { 'edpoprec': EDPOPREC, 'edpopcol': EDPOPCOL, 'rdfs': RDFS }
-#         # results = g.query(query, initNs=namespaces)
-#         # return results
+    def _triples_to_store(self, g: Graph, instance: RDFModel, value: Iterable[IdentifiedNode]) -> Triples:
+        return [
+            (uri, RDFS.member, instance.uri)
+            for uri in value
+        ]
 
-#     def _stored_triples(self, g: Graph, instance) -> Triples:
-#         return g.triples((None, RDFS.member, instance.uri))
 
 class EDPOPCollection(RDFModel):
     rdf_class = EDPOPCOL.Collection
 
     name = RDFUniquePropertyField(AS.name)
     summary = RDFUniquePropertyField(AS.summary)
-    # projects = ProjectContextField()
+    projects = ProjectContextField()
+    records = CollectionMembersField()
--- backend/collect/rdf_models_test.py
+++ backend/collect/rdf_models_test.py
@@ -1,7 +1,8 @@
 import pytest
-from rdflib import URIRef
+from rdflib import URIRef, RDF, RDFS
 from django.conf import settings
 
+from triplestore.constants import AS, EDPOPCOL
 from projects.models import Project
 from projects.rdf_models import RDFProject
 from collect.rdf_models import EDPOPCollection
@@ -17,4 +18,21 @@ def test_collection_model(project):
 
     collection = EDPOPCollection(project.graph, uri)
     collection.name = 'Test collection'
+    collection.projects = [project]
+    collection.records = [
+        URIRef('https://example.org/example1'),
+        URIRef('https://example.org/example2')
+    ]
     collection.save()
+
+    store = settings.RDFLIB_STORE
+
+    assert any(store.triples((collection.uri, RDF.type, EDPOPCOL.Collection)))
+    assert any(store.triples((collection.uri, AS.context, project.uri)))
+    assert any(store.triples((None, RDFS.member, collection.uri)))
+
+    collection.delete()
+
+    assert not any(store.triples((collection.uri, RDF.type, EDPOPCOL.Collection)))
+    assert not any(store.triples((collection.uri, AS.context, project.uri)))
+    assert not any(store.triples((None, RDFS.member, collection.uri)))
