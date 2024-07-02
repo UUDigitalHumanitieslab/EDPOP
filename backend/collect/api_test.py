5596d1bc370330c193dd523ab6658ac0854d002b
Luka van der Plas <lukavdplas@mailfence.com>
2024-07-02 16:34:07 +0200

    outfactor creation function

--- backend/collect/api_test.py
+++ backend/collect/api_test.py
@@ -6,15 +6,18 @@ from urllib.parse import quote
 
 from triplestore.constants import EDPOPCOL
 
-def test_create_collection(db, user, project, client: Client):
-    client.force_login(user)
-
+def post_collection(client, project_name):
     data = {
         'name': 'My collection',
         'summary': 'These are my favourite records',
-        'project': project.name,
+        'project': project_name,
     }
-    response = client.post('/api/collections/', data, content_type='application/json')
+    return client.post('/api/collections/', data, content_type='application/json')
+
+def test_create_collection(db, user, project, client: Client):
+    client.force_login(user)
+
+    response = post_collection(client, project.name)
     assert is_success(response.status_code)
     uri = URIRef(response.data['uri'])
 
@@ -30,35 +33,22 @@ def test_list_collections(db, user, project, client: Client):
     assert is_success(response.status_code)    
     assert len(response.data) == 0
 
-    data = {
-        'name': 'My collection',
-        'summary': 'These are my favourite records',
-        'project': project.name,
-    }
-    response = client.post('/api/collections/', data, content_type='application/json')
+    response = post_collection(client, project.name)
 
     response = client.get('/api/collections/')
     assert is_success(response.status_code)    
     assert len(response.data) == 1
-    assert response.data[0] == {
-        'uri': settings.RDF_NAMESPACE_ROOT + 'collections/my_collection',
-        **data,
-    }
+    assert response.data[0]['uri'] == settings.RDF_NAMESPACE_ROOT + 'collections/my_collection'
+    assert response.data[0]['name'] == 'My collection'
 
 
 def test_retrieve_collection(db, user, project, client: Client):
     client.force_login(user)
-    data = {
-        'name': 'My collection',
-        'summary': 'These are my favourite records',
-        'project': project.name,
-    }
-    response = client.post('/api/collections/', data, content_type='application/json')
+
+    response = post_collection(client, project.name)
     uri = response.data['uri']
 
     retrieve_url = '/api/collections/{}/'.format(quote(uri, safe=''))
-    print(retrieve_url)
     response = client.get(retrieve_url)
     assert is_success(response.status_code)
-
     assert response.data['name'] == 'My collection'
