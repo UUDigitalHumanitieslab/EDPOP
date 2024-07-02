f3a858a205da8ff713499a8113d9d603555d6c74
Tijmen Baarda <tijmenbaarda@gmail.com>
2024-07-02 14:50:01 +0200

    Revert "Use makeLinkEnabler from backbone-util"
    This reverts commit a71151953c8080b9b37aea40c3fe274b70254a9c.

--- frontend/vre/catalog/select-catalog.view.js
+++ frontend/vre/catalog/select-catalog.view.js
@@ -20,4 +20,11 @@ export var SelectCatalogView = Backbone.View.extend({
         };
         this.$el.html(this.template(context));
     },
+    select: function(event) {
+        event.preventDefault();
+        var href = $(event.target).attr('href');
+        Backbone.history.navigate(href, true);
+        var selectedDB = event.target.innerText;
+        this.$el.html(this.template({'selected-db': selectedDB, 'catalogs': this.getCatalogs()}));
+    },
 });
