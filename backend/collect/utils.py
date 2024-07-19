f6cee0c29dc1b5f128a8eb7fb8c4bcc1927852b3
Luka van der Plas <lukavdplas@mailfence.com>
2024-07-19 13:24:26 +0200

    update name_to_slug

--- backend/collect/utils.py
+++ backend/collect/utils.py
@@ -5,12 +5,11 @@ import re
 from triplestore.constants import EDPOPCOL
 
 def _name_to_slug(name: str) -> str:
-    no_spaces = lambda s: re.sub(r'\s+', '_', s)
-    clean = lambda s: re.sub(r'[^a-z0-9\-_]', '', s)
-    strip_start = lambda s: re.sub(r'^\W+', '', s)
-    strip_end = lambda s: re.sub(r'\W+$', '', s)
-
-    return strip_end(strip_start(clean(no_spaces(str.lower(name)))))
+    lowered = name.lower()
+    cleaned = re.sub(r'[^a-z0-9\-_\s]', '', lowered)
+    stripped = re.sub(r'(\W+$|^\W+)', '', cleaned)
+    no_spaces = re.sub(r'\s+', '_', stripped)
+    return no_spaces
 
 
 def collection_uri(name: str):
