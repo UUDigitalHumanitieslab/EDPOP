0fd5d9f5109c276c73c6abc4e376f7983922154a
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-07-23 16:59:43 +0200

    Patch FilteredCollection to reuse modelId of underlying collection
    See CentreForDigitalHumanities/backbone-collection-transformers#2.

--- frontend/vre/utils/filtered.collection.js
+++ frontend/vre/utils/filtered.collection.js
@@ -6,3 +6,8 @@ import { deriveFiltered } from '@uu-cdh/backbone-collection-transformers';
  * @extends Collection
  */
 export var FilteredCollection = deriveFiltered();
+
+// Temporary workaround for backbone-collection-transformers#2.
+FilteredCollection.prototype.modelId = function(attrs, idAttribute) {
+    return this._underlying.modelId(attrs, idAttribute);
+};
