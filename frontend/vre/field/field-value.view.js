7b4efc8f17302d589bbad71ceb0c10be9d8d41e4
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-07-31 02:12:07 +0200

    Add new view for field multivalues (#293 #353)

---
+++ frontend/vre/field/field-value.view.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import { View } from '../core/view.js';
+import fieldValueTemplate from './field-value.view.mustache';
+
+function bubble(eventName) {
+    return function(event) {
+        event.preventDefault();
+        this.model.trigger(eventName, this.model, this, event);
+    };
+}
+
+export var FieldValueView = View.extend({
+    tagName: 'tr',
+    template: fieldValueTemplate,
+
+    events: {
+        'click .fa-plus, .fa-pen': bubble('edit'),
+        'click .fa-link-slash': bubble('requestRelink'),
+        'click .fa-xmark': bubble('discard'),
+    },
+
+    initialize: function() {
+        this.render().listenTo(this.model, 'change', this.render);
+    },
+
+    render: function() {
+        var attributes = this.model.attributes,
+            fieldInfo = attributes.isFirst && this.model.getFieldInfo(),
+            linkedUri = this.model.getLinkedUri(),
+            linkedRecordUri = linkedUri && encodeURIComponent(linkedUri),
+            edit = attributes.edit,
+            updatedDate = edit && edit.getUpdatedDate(),
+            payload = _.extend({}, attributes, {
+                fieldInfo,
+                linkedRecordUri,
+                updatedDate,
+            });
+        this.$el.html(this.template(payload));
+        return this;
+    },
+});
